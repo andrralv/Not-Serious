7a8de1b4febe88bd19b2ef87aa0fdcbd2ff1dc36
andralv <jdest77@gmail.com>
2016-01-22 16:44:50 -0600

    Adding database info

---
+++ mysql/scrDML.sql
@@ -0,0 +1,13 @@
+INSERT INTO PARTICIPANTE(idPARTICIPANTE, nombre, apellido, nacionalidad, especialidad, esponente) VALUES
+(01255, 'carlos', 'azofeifa', 'costarricense', 'ingeniero', True),
+(02632, 'manuel', 'morales', 'costarricense', 'arquitecto', True),
+(02548, 'jacqueline', 'gillet', 'frances', 'paisajista', True),
+(02156, 'johnny', 'perez', 'costarricense', 'urbanista', False),
+(15845, 'miguel', 'boschini', 'italiano', 'arquitecto', False),
+(12165, 'laura', 'chaverri', 'costarricense', 'paisajista', True)
+
+
+UPDATE OYENTE SET ponencia='Arquitectura y Urbanismo en Costa Rica'
+WHERE ponencia=Null
+
+SELECT * FROM PARTICIPANTE
